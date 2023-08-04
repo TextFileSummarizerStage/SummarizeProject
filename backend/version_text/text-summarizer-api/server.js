255f2d649a73a883f41935f997103a81b04a8934
naderouali <oualinader@gmail.com>
2023-08-04 11:49:49 +0100

    first commit

--- backend/version_text/text-summarizer-api/server.js
+++ backend/version_text/text-summarizer-api/server.js
@@ -5,24 +5,51 @@ const axios = require('axios');
 const app = express();
 const port = 6000;
 
+const {spawn} = require('child_process');
+
 app.use(bodyParser.json());
 
 app.post('/summarize', async (req, res) => {
-  try {
-    const { text, num_sentences } = req.body;
+    try {
+        const pythonProcess = spawn('python', ['path/to/your/python_script.py']);
 
-    const response = await axios.post('http://localhost:5000/summarize', {
-      text,
-      num_sentences,
-    });
+        const dataToSend = JSON.stringify({text, num_sentences});
+        pythonProcess.stdin.write(dataToSend);
+        pythonProcess.stdin.end();
 
-    res.json({ summary: response.data.summary });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred.' });
-  }
+        let pythonResponse = '';
+        pythonProcess.stdout.on('data', (data) => {
+            pythonResponse += data;
+        });
+
+        let pythonError = '';
+        pythonProcess.stderr.on('data', (data) => {
+            pythonError += data;
+        });
+
+        pythonProcess.on('close', (code) => { // ... handle the Python script's response and errors
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'An error occurred.', error});
+    }
 });
 
+// app.post('/summarize', async (req, res) => {
+// try {
+//     const { text, num_sentences } = req.body;
+//     const response = await axios.post('http://localhost:5000/summarize', {
+//       text,
+//       num_sentences,
+//     });
+
+//     res.json({ summary: response.data.summary });
+// } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'An error occurred.', error });
+// }
+// });
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
